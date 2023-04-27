566881198662e28b40666374d53486eb05535f70
Abigail Noyes <abbeynoyes@gmail.com>
2023-04-26 21:07:47 -0400

    Building an exe file

--- mini_project1_quiz_game.py
+++ mini_project1_quiz_game.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python3
+
 print("Welcome to my computer quiz")
 
 playing = input("Do you want to play? ")
 
 if playing != "yes":
     quit()
-    
+  
 print("Okay! Lets play :) ")
-
+score = 0  
 # question 1
 answer = input("What does CPU stand for? ")
 if answer == "central processing unit":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect!")
     
@@ -18,6 +21,7 @@ else:
 answer = input("What does GPU stand for? ")
 if answer == "graphics processing unit":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect!")
     
@@ -25,6 +29,7 @@ else:
 answer = input("What does RAM stand for? ")
 if answer == "random access memory":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect!")
     
@@ -32,5 +37,9 @@ else:
 answer = input("What does PSU stand for? ")
 if answer == "power supply":
     print("Correct!")
+    score += 1
 else: 
     print("Incorrect!")
+    
+print("You got " + str(score) + " questions correct!")
+print("You got a " + str((score/4) * 100) + "%.")
